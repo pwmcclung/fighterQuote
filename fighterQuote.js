f279141cb60f50c63feca5f3f84997507f4a1abc
pwmcclung <78838481+pwmcclung@users.noreply.github.com>
2022-04-05 06:36:08 -0600

    Create fighterQuote.js

---
+++ fighterQuote.js
@@ -0,0 +1,8 @@
+var quote = function(fighter) {
+  let fighterL = fighter.toLowerCase()
+  if (fighterL ==='george saint pierre'){
+    return 'I am not impressed by your performance.'
+  }else{
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+};
